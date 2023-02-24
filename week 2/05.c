695e23ee6b3b97cd03be579751764156024376e5
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-02-24 11:55:51 +0800

    Create 05.c

---
+++ week 2/05.c
@@ -0,0 +1,17 @@
+//用const定義常數
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	const int salary = 34500; //宣告salary為常數
+  printf("salary = %d\n",salary);
+	{
+		const int salary = 13500; //在此程式區塊中宣告salary為常數
+		printf("salary = %d\n",salary);
+	}
+
+	printf("salary = %d\n",salary);
+	
+  return 0;
+}
