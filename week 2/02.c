16748f53f0c67d564f4f6bee5e90469219fd61d1
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-02-24 11:54:19 +0800

    Create 02.c

---
+++ week 2/02.c
@@ -0,0 +1,15 @@
+//變數的宣告
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a;
+	int b = 12;
+	float _c = 117.123456;
+  
+	printf("變數 a = %d\n",a); //列印為初始化的 整數a值
+  printf("變數 b = %d\n",b); //列印為已始化的 整數b值
+  printf("變數 _c = %f\n",_c); //列印為已始化的 浮點數_c值
+
+  return 0;
+}
