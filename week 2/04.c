26263b7644ca53063a33da6f2b39377f7b77b207
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-03 02:19:49 +0800

    Update 04.c

--- week 2/04.c
+++ week 2/04.c
@@ -6,9 +6,10 @@
 //宣告PI = 3.14159
 
 int main(void) {
-  float radius=5.0,Area;	//宣告與設定圓半徑
+  	float radius=5.0,Area;	//宣告與設定圓半徑
 	Area = radius*radius*PI; //計算圓面積
 
 	printf("圓的半徑 = %f\n面積 = %f\n",radius,Area);
-  return 0;
+  
+	return 0;
 }
