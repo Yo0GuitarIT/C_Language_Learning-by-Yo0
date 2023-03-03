59363a7cc299ba8a267f6a765e4195cd8cf4d63a
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-03 16:11:44 +0800

    Create 06.c

---
+++ Week 3/06.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	char ch = 042; //雙引號的八進位ASCII碼
+
+	//印出字元變數
+	printf("列印出八進位042所代表的字元符號 --> %c \n",ch);
+	printf("雙引號的應用--> %c Greenergy %c\n",ch,ch);
+	
+  return 0;
+}
