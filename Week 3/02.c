2705255d6bc761145dc8bc7dd578efae9f2024c3
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-03 15:42:15 +0800

    Create 02.c

---
+++ Week 3/02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  float f1 = 123.4568357109375F; //宣告單精數浮點數
+	float f2 = 21341372.1357912; //宣告八位整數值的單精數浮點數
+	double d1 = 123456789.123456789123; //宣告倍精數浮點數
+
+	printf("f1=%f\n",f1);
+	printf("f2=%f\n",f2);
+	printf("d1=%f\n",d1);
+  
+		return 0;
+}
