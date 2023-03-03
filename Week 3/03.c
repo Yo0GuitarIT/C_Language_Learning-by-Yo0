a4bb65ad8823a5f81627c8cd8451eeed100efe57
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-03 15:45:14 +0800

    Create 03.c

---
+++ Week 3/03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  float f1 = 0.654321;
+	float f2 = 5467.1234;
+
+	printf("f1=%f=%e\n",f1,f1);
+	printf("f2=%f=%e\n",f2,f2);
+  
+		return 0;
+}
