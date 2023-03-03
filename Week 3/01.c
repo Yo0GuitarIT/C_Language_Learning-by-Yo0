e4c458b25969255b05401721933faa840df1f787
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-03 15:26:20 +0800

    Create 01.c

---
+++ Week 3/01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  
+  unsigned short int s1 = -1; //超過無號短整數的下限值
+	short int s2 = 32768; //超過短整數的上限值
+
+	printf("s1=%d\n",s1);
+	printf("s2=%d\n",s2);
+	
+  return 0;
+}
