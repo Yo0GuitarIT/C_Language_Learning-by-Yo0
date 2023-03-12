6908305e5e8cbf343b1a262beaae944ede137d3c
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-13 01:26:25 +0800

    Update 07.c

--- Week 4/07.c
+++ Week 4/07.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 int main(void) {
-	
+	/*
   	float f1,f2;
 
-	scanf("%f%f",&f1,&f2); //用空白間隔兩數
+	scanf("%f%f",&f1,&f2); //用enter間隔兩數
 	printf("%f\n",f1+f2);
   
 	printf("---------------------\n");
@@ -18,6 +18,16 @@ int main(void) {
 	printf("n2:");
 	scanf("%d",&n2);
 	printf("n2=%d\n",n2);
+  	*/
+	printf("---------------------\n");
+	char c1,c2;
+	
+	printf("輸入兩個字母：");
+	scanf("%c%c",&c1,&c2);
+
+	printf("c1=%c ASCII=%d\n",c1,c1);
+	printf("c2=%c ASCII=%d",c2,c2);
+	
 
 	return 0;
 }
