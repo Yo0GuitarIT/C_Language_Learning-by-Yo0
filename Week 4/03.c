8d0f81a8a540c70c32a87de40d9767c0d1ee8040
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-13 00:07:59 +0800

    Update 03.c

--- Week 4/03.c
+++ Week 4/03.c
@@ -13,6 +13,12 @@ int main(void) {
 	printf("no=%*d\n",6,no);
 	printf("no=%*d\n",8,no);
 	//另外一種寫法
+	printf("-------------------\n");
+	printf("no=%d\n",no);
+	printf("no=%.6d\n",no); //精度設定為.6
+	printf("no=%.8d\n",no); //精度設定為.6
+	printf("no=%.2d\n",no); //精度設定小於原本的顯示字數
+	printf("no=%8.2d\n",no); //８表示預留８個字元寬度
 	
 	return 0;
 }
