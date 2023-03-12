18a9bcfcedb290bfc9e9fe27b873d463a6ae5917
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-13 00:30:12 +0800

    Create 06.c

---
+++ Week 4/06.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int val = 345;
+
+	printf("%%d 格式輸出結果=%d\n",val);
+	printf("+6d 格式輸出結果=%+6d\n",val);
+	printf("-6d 格式輸出結果=%-6d\n",val);
+	printf("+#6o 格式輸出結果=%+#6o\n",val);
+	printf("+#6x 格式輸出結果=%+#6x\n",val);
+	printf("06d 格式輸出結果=%06d\n",val);
+  return 0;
+}
