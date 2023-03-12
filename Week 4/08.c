aab3d4c7cd1f9e1c9f604060be7653e0391f0886
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-13 01:39:11 +0800

    Update 08.c

--- Week 4/08.c
+++ Week 4/08.c
@@ -11,5 +11,19 @@ int main(void) {
 	printf("Second num = %d\n",last);
 	printf("sum = %d",first+last);
 
+	printf("---------------------\n");
+
+	char name1[10],name2[10],name3[10];
+	printf("input a string:");
+	scanf("%s",name1);
+	printf("input two string:");
+	scanf("%s%s",name2,name3);
+
+	printf("name1=%s\n",name1);
+	printf("name2=%s\n",name2);
+	printf("name3=%s\n",name3);
+	
+	
+
   return 0;
 }
