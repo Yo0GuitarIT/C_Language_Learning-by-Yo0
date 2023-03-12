0f3bb63002b5a53c1f71bb2ec964d35a0fce6693
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-12 23:50:48 +0800

    Create 02.c

---
+++ Week 4/02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int num = 138;
+
+	printf("num 的八進位數=%o\n",num); 
+	//以%o格式化字元輸出
+	printf("num 的八進位數=%x\n",num);
+	//以%x格式化字元輸出
+	printf("num 的八進位數=%X\n",num);
+	//以%X格式化字元輸出
+
+	
+	return 0;
+}
