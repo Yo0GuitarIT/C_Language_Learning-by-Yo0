abf93d8e104481e3a77ac05a1a93a198f5b36a65
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-17 09:36:57 +0800

    Update 02.c

--- Week 4/02.c
+++ Week 4/02.c
@@ -4,24 +4,21 @@
 int main(void) {
   	int num = 138;
 
-	printf("num 的八進位數=%o\n",num); 
-	//以%o格式化字元輸出
-	printf("num 的八進位數=%x\n",num);
-	//以%x格式化字元輸出
-	printf("num 的八進位數=%X\n",num);
-	//以%X格式化字元輸出
+	printf("num 的八進位數=%o\n",num); //以%o格式化字元輸出
+	printf("num 的十六進位數=%x\n",num); //以%x格式化字元輸出
+	printf("num 的十六進位數=%X\n",num); //以%X格式化字元輸出
 	printf("-------------------------------\n");
 
 	float value1 = 123.456;
 	float value2 = 1234567.1234;
 
 	printf("value1的f格式輸出= %f\n",value1);
-	printf("value1的e格式輸出= %e\n",value1);
-	printf("value1的g格式輸出= %g\n",value1);
+	printf("value1的e格式輸出= %e\n",value1); //科學記號
+	printf("value1的g格式輸出= %g\n",value1); //輸出有效六位數
 	printf("-------------------------------\n");
 	printf("value2的f格式輸出= %f\n",value2);
 	printf("value2的e格式輸出= %e\n",value2);
-	printf("value2的g格式輸出= %g\n",value2);
+	printf("value2的g格式輸出= %g\n",value2); //輸出有效六位數，轉科學記號
 
 	
 	return 0;
