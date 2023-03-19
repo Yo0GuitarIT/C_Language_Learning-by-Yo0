1efc42fa8ff07b9484aef57e7dcbff8e94ca8076
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-20 00:15:48 +0800

    Create 01.c

---
+++ Week 5/01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int a =125;
+
+	//125對4,5,6的餘數運算
+	printf("%d%%4=%d\n",a,a%4); //輸出１２５％４
+	printf("%d%%5=%d\n",a,a%5);
+	printf("%d%%6=%d\n",a,a%6);
+  
+	return 0;
+}
