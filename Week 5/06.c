4b2df5e8e3e7d76cd59c19c87e0fd4f55a586d58
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-20 00:57:33 +0800

    Create 06.c

---
+++ Week 5/06.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int a,b;
+
+	a=15;
+	printf("a=%d\n",a);
+	b=++a; //前置型遞增運算子
+	printf("b=++a  -->  a=%d,b=%d\n",a,b);
+
+	a=15;
+	printf("a=%d\n",a);
+	b=a++; //後置型遞增運算子
+	printf("b=a++  -->  a=%d,b=%d\n",a,b);
+
+	a=15;
+	printf("a=%d\n",a);
+	b=--a; //前置型遞減運算子
+	printf("b=--a  -->  a=%d,b=%d\n",a,b);
+
+	a=15;
+	printf("a=%d\n",a);
+	b=a--; //後置型遞減運算子
+	printf("b=a--  -->  a=%d,b=%d\n",a,b);
+	
+  return 0;
+}
