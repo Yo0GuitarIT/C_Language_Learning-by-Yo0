dd31b58b36e2eb1bf251ba83254de5844771400b
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-20 00:38:03 +0800

    Create 04.c

---
+++ Week 5/04.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int a=12,b=38;
+
+	printf("%d&%d=%d\n",a,b,a&b); //AND &
+	printf("%d|%d=%d\n",a,b,a|b); //OR |
+	printf("%d^%d=%d\n",a,b,a^b); //XOR ^
+	printf("~%d=%d\n",a,~a); //NOT ~
+	
+  return 0;
+}
