e2e0631b824a1987f984b3599da6500202006fe5
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-20 00:22:07 +0800

    Create 02.c

---
+++ Week 5/02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a=19,b=13;
+
+	printf("a=%d b=%d \n",a,b);
+	printf("------------------------------\n");
+	printf("a>b,比較結果為 %d 值\n",a>b);
+	printf("a<b,比較結果為 %d 值\n",a<b);
+	printf("a>=b,比較結果為 %d 值\n",a>=b);
+	printf("a<=b,比較結果為 %d 值\n",a<=b);
+	printf("a==b,比較結果為 %d 值\n",a==b);
+	printf("a!=b,比較結果為 %d 值\n",a!=b);
+	
+  return 0;
+}
