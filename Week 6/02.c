2af280b5f1987e97bd9a64a2571e9e67d4db529a
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-25 21:42:46 +0800

    Create 02.c

---
+++ Week 6/02.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int value;
+
+	printf("Input one integer number: ");
+	scanf("%d",&value);
+	if(value%2==0 && value%3==0){
+		if(value%5==0)
+			printf("Conform!");
+		else
+			printf("Not conform! for 5");
+	}
+	else
+		printf("Not cconform all");
+  return 0;
+}
