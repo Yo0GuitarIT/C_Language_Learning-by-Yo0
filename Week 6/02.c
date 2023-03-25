ded89f769fd991d9939f4c204a1a57a033110a03
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-25 21:44:03 +0800

    Update 02.c

--- Week 6/02.c
+++ Week 6/02.c
@@ -8,7 +8,7 @@ int main(void) {
 	scanf("%d",&value);
 	if(value%2==0 && value%3==0){
 		if(value%5==0)
-			printf("Conform!");
+			printf("Conform!"); //單行內容不用{...}
 		else
 			printf("Not conform! for 5");
 	}
