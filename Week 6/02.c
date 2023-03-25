e9d770035dc038bff02e457df4f32ada7986e8c7
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-25 21:53:16 +0800

    Update 02.c

--- Week 6/02.c
+++ Week 6/02.c
@@ -2,17 +2,20 @@
 #include <stdlib.h>
 
 int main(void) {
-	int value;
+ 	int value;
 
 	printf("Input one integer number: ");
 	scanf("%d",&value);
-	if(value%2==0 && value%3==0){           //判斷是否為2和3的倍數
-		if(value%5!=0)                  //判斷value是否不為5的倍數
-			printf("Conform!");     //單行內容不用{...}
-		else
+	if(value%2==0 && value%3==0){            //判斷是否為2和3的倍數
+		if(value%5!=0){                  //判斷value是否不為5的倍數
+			printf("Conform!");
+		}                                //單行內容可不用{...} 但建議養成使用習慣
+		else{
 			printf("Not conform! for 5");
+		}
 	}
-	else
+	else{
 		printf("Not cconform all");
+	}
   return 0;
 }
