dea974623c4770d5f752fb0cc3baeae5d274eadb
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-25 19:19:47 +0800

    Update 06.c

--- Week 5/06.c
+++ Week 5/06.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(void) {
-  	int a,b;
+  int a,b;
 
 	a=15;
 	printf("a=%d\n",a);
@@ -23,6 +23,12 @@ int main(void) {
 	printf("a=%d\n",a);
 	b=a--; //後置型遞減運算子
 	printf("b=a--  -->  a=%d,b=%d\n",a,b);
+
+	a=a+1;  
+	a+=1;	
+	a++;
+	++a;
+	//以上皆是遞增的方法
 	
   return 0;
 }
