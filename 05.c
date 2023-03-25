d4cc8072f3767bd82985d5fd2aada37ca8a6a038
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-03-25 22:43:14 +0800

    Create 05.c

---
+++ 05.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  char select;
+  printf(" (1)排骨便當\n");
+	printf(" (2)海鮮便當\n");
+	printf(" (3)雞腿便當\n");
+  printf(" (4)魚排便當\n");
+	printf("\n=========================\n");
+	printf("請輸入您要購買的便當: ");
+	scanf("%c",&select);
+
+	switch(select){
+		case '1':
+			printf("排骨便當一份75元");
+			break;  //跳出迴圈
+		case '2':
+			printf("海鮮便當一份85元");
+			break;
+		case '3':
+			printf("雞腿便當一份90元");
+			break;
+		case '4':
+			printf("魚排便當一份95元");
+			break;
+		default:  //select內容為滿足於case選項時執行
+		  printf("輸入錯誤...");
+	}
+	printf("\n=========================\n");
+  return 0;
+}
