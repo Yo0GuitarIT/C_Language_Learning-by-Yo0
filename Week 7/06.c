ab6ba6cd8612bff429ed2997cdadcd78a0488c85
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-04-10 16:41:30 +0800

    Create 06.c

---
+++ Week 7/06.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int a,b;
+	for(a=1;a<=9;a++){
+		for(b=1;b<=a;b++){
+		if(b==6)
+			continue;
+		//printf("%d",b);
+		printf(" * ");	
+		}
+		printf("\n");
+	}
+  return 0;
+}
