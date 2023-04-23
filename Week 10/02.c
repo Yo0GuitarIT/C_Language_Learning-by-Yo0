6a32a40b738f31563e45a1d76ab7640c847ce303
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-04-23 23:03:48 +0800

    Create 02.c

---
+++ Week 10/02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int arr[10]={1,2,3,4,5,6,7,8,9,10};
+	int sum = 0;
+
+	for(int i=0;i<10;i++){
+		if(i==0)
+			printf(" "); //i=0時 輸出空格
+		else
+			printf("+"); //i!=0時 輸出"+"
+		
+		sum += arr[i]; //將陣列中每元素加到sum
+		
+		printf("%d=%d\n",arr[i],sum);
+	}
+	
+  return 0;
+}
