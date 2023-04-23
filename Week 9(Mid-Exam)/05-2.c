4d017ef1ade7b7c1766ba82f073afbc6daf507f4
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-04-23 22:02:35 +0800

    Create 05-2.c

---
+++ Week 9(Mid-Exam)/05-2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int n,prime;
+	int counter = 0;
+
+	printf("Input a number: ");
+	scanf("%d",&n);
+	printf("n=%d\n",n);
+	printf("prime is ");
+
+	for(int i=2;i<=n;i++){
+		for(int j=2;j<=i;j++){
+			if(i%j==0)
+				counter+=1;
+		}
+		if(counter==1){
+			prime=i;
+			printf("%d ",prime);
+			counter=0;
+		}
+		else{
+			counter=0;
+		}
+	}
+  return 0;
+}
