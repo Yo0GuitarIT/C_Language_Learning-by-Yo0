7f9f4b1e8de2e3fbb9bf7a950464d8cd2d6cba4c
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-04-25 01:50:45 +0800

    Create 05.c

---
+++ Week 11/05.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+  char str1[40],str2[40];
+
+	printf("請輸入第一個字串：");
+	scanf("%s",str1);
+	printf("請輸入第二個字串：");
+	scanf("%s",str2);
+
+	strcat(str1,str2); //將兩字串聯結
+	printf("str1+str2= %s\n",str1);
+	
+	printf("Reverse-->");
+	for(int i=strlen(str1)-1;i>=0;i--){
+		printf("%c",str1[i]);
+	}
+	
+  return 0;
+}
