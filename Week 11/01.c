501212bef1266ee164ceb7de7962a620947a7cc7
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-04-25 01:50:57 +0800

    Update 01.c

--- Week 11/01.c
+++ Week 11/01.c
@@ -4,7 +4,7 @@
 int main(void) {
 
 	//四種字串宣告 與 初始值設定模式
-  char str_1[6]="Hello";
+  	char str_1[6]="Hello";
 	char str_2[]={'H','e','l','l','o','\0'};
 	char str_3[]="hello";
 	char str_4[]={'H','e','l','l','o','!'};
