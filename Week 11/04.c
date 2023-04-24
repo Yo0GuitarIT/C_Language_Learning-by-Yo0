bd6a2875a719451b32184c3141e04b8732afadac
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-04-25 01:50:19 +0800

    Create 04.c

---
+++ Week 11/04.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  char S1[30],S2[30],S3[60];
+	int record=0;
+
+	printf("字串S1的內容：");
+	scanf("%s",S1);
+	printf("字串S2的內容：");
+	scanf("%s",S2);
+
+	for(int count=0;S1[count]!='\0';count++,record++){
+		S3[record]=S1[count];
+	}
+
+	for(int count=0;S2[count]!='\0';count++,record++){
+		S3[record]=S2[count];
+	}
+
+	S3[record]+='\0';
+
+	printf("連結後的字串S3:%s",S3);
+	printf("\n");
+	
+  return 0;
+}
