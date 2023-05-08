68285d254a0461b36cda442344e0666ebd6fd815
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-05-08 18:35:38 +0800

    Create 04.c

---
+++ Week 13/04.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int *ptr,no; //宣告整數型態指標ptr和變數no
+	ptr=&no;     //初始化指標
+
+	printf("最初的位置：%p\n",ptr);
+	
+	ptr++;
+	printf("ptr++後的位置：%p\n",ptr);
+	
+	ptr--;
+	printf("ptr--後的位置：%p\n",ptr);
+
+	ptr+=2;
+	printf("ptr+=2後的位置：%p\n",ptr);
+
+	ptr-=2;
+	printf("ptr-=2後的位置：%p\n",ptr);
+  return 0;
+}
