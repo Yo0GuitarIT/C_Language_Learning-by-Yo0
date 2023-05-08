601766cb6b2ee42361d020ed367e281513d745ed
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-05-08 19:39:52 +0800

    Update 06.c

--- Week 13/06.c
+++ Week 13/06.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-  char name[15];
-	char *number="Please input your name:";
+ 	char name[15];
+ 	char *number="Please input your name:";
 	printf("%s",number);
 	scanf("%s",&name);
 	printf("Your name is %s\n",name);
