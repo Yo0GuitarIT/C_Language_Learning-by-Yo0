661ccfe2392903fb93a999f7bcb15b2f4c35508d
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-05-14 00:08:10 +0800

    Create 01.c

---
+++ Week 14/01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  float* piF = (float*)malloc(sizeof(float));
+
+	printf("輸入piF的值：\n");
+	scanf("%f",piF);
+	printf("piF所指的地址內容為%f\n",*piF);
+	printf("piF所指的地址為%p\n",piF);
+
+	free(piF);
+  return 0;
+}
