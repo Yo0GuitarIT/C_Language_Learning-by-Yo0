7f5de62e78be676c1b5eb3d9566c97be04f88aa7
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-05-15 19:32:39 +0800

    Create 03.c

---
+++ Week 14/03.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+  char *name;
+	int i;
+
+	printf("請輸入英文字串長度：");
+	scanf("%d",&i);
+
+	name = (char *)malloc((i+1)*sizeof(char));
+	//i+1 -->'\0'加入字串最後端
+
+	printf("請輸入英文字串：");
+	scanf("%s",name);
+	strcat(name,"\0");
+	printf("-%s-\n",name);
+
+	printf("字串長度：%d\n",strlen(name));
+  return 0;
+}
