93bbbe78288a4389a70389e88ea4f05f9f9151cb
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-05-17 19:29:39 +0800

    Create 06.c

---
+++ Week 14/06.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  void *p = (void *)100; //void *p指標原始值為(void *)型態的值為100
+	printf("Adress: %p\n",p);          //(void *)100的地址
+	printf("Integer: %d\n",(int *)p);  //將(void *)100以整數型態轉型 輸出值=100
+	printf("Character: %c",(char *)p); //將(void *)100以字元型態轉型 輸出值='d'(ASCII)
+  return 0;
+}
