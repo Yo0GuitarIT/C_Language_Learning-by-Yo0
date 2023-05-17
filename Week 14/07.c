340a29b398e62da0edb22d051d9cff5f0c616efc
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-05-17 19:57:02 +0800

    Create 07.c

---
+++ Week 14/07.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int mymax(int,int); //函數原形宣告
+
+int main(void) {
+  int a,b;
+	printf("數字比大小\n");
+	
+	printf("數字請輸入a:");
+	scanf("%d",&a);
+	printf("數字請輸入b:");
+	scanf("%d",&b);
+
+	printf("較大的數：%d",mymax(a,b)); //呼叫mymax()函數
+	
+  return 0;
+}
+
+int mymax(int x,int y){   //函數定義主體
+		if(x>y)
+			return x;
+		else
+			return y;
+	}
