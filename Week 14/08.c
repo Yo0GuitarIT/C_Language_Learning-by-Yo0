34668d7de087f7f4cc3cccdbeb11323015363a20
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-05-17 19:58:36 +0800

    Update 08.c

--- Week 14/08.c
+++ Week 14/08.c
@@ -4,13 +4,13 @@
 void swap_test(int,int); //傳值呼叫函數
 
 int main(void) {
-  int a=10,b=20;
+  	int a=10,b=20;
 
 	printf("函數外交換前，a=%d,b=%d\n\n",a,b);
 	swap_test(a,b);
 	printf("函數外交換後，a=%d,b=%d\n",a,b);
 	
-  return 0;
+  	return 0;
 }
 
 void swap_test(int x,int y){ //未回傳值
