5036aca08968d39344b618fdc5daa6b5a60c1bcf
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-05-17 19:29:04 +0800

    Update 05.c

--- Week 14/05.c
+++ Week 14/05.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(void) {
-  char **month;
+  	char **month;
 	int row,col;
 	char month_name[12][10]={"January","February","March","April","May","June","July","August","September","October","Novermber","December"};
 	
