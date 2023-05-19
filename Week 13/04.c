d9b1f307dc55b928296458a4d64019813be35d66
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-05-19 09:48:25 +0800

    Update 04.c

--- Week 13/04.c
+++ Week 13/04.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 
 int main(void) {
-  	int *ptr,no; //宣告整數型態指標ptr和變數no
-	ptr=&no;     //初始化指標
+  int *ptr,no=10; //宣告整數型態指標ptr和變數no
+	ptr=&no;     //初始化指標  
 
-	printf("最初的位置：%p\n",ptr);
+	printf("最初的位置：%p *ptr=%d\n",ptr,*ptr); //*ptr=no
 	
 	ptr++;
-	printf("ptr++後的位置：%p\n",ptr);
+	printf("ptr++後的位置：%p *ptr=%d\n",ptr,*ptr);
 	
 	ptr--;
-	printf("ptr--後的位置：%p\n",ptr);
+	printf("ptr--後的位置：%p *ptr=%d\n",ptr,*ptr);
 
-	ptr+=2;
-	printf("ptr+=2後的位置：%p\n",ptr);
+	ptr+=2; //ptr = ptr+2
+	printf("ptr+=2後的位置：%p *ptr=%d\n",ptr,*ptr);
 
 	ptr-=2;
-	printf("ptr-=2後的位置：%p\n",ptr);
+	printf("ptr-=2後的位置：%p *ptr=%d\n",ptr,*ptr);
   return 0;
 }
