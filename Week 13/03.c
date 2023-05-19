72271d55be2a034c17228565fac38e4996f2665c
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-05-19 09:43:27 +0800

    Update 03.c

--- Week 13/03.c
+++ Week 13/03.c
@@ -11,7 +11,7 @@ int main(void) {
 
 	printf("變數a1之地址:%p ,內容:%d\n",&a1,a1);
 	printf("變數ptr1之地址:%p ,內容:%p ,*ptr1:%d\n",&ptr1,ptr1,*ptr1);
-	printf("變數ptr2之地址:%p ,內容:%p ,**ptr2:%d\n",&ptr2,ptr3,**ptr2);
+	printf("變數ptr2之地址:%p ,內容:%p ,**ptr2:%d\n",&ptr2,ptr2,**ptr2);
 	printf("變數ptr3之地址:%p ,內容:%p ,***ptr3:%d\n",&ptr3,ptr3,***ptr3);
   return 0;
 }
