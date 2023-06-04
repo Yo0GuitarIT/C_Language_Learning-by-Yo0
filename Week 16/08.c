089a69fff71c0522e2b32e86d4e5e30dab824042
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-06-04 16:30:24 +0800

    Create 08.c

---
+++ Week 16/08.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.14159
+#define SHOW "The Circle' Area = "
+#define RESULT r*r*PI
+
+int main(void) {
+  int r;
+	
+	printf("Input thr radius r:");
+	scanf("%d",&r);
+	printf(SHOW"%f",RESULT);
+  return 0;
+}
